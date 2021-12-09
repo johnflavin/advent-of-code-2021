4837acf8939d0f20641991312869ee8b799b881b
John Flavin <flavinj@gmail.com>
2021-12-09 10:22:58 -0600

    Add placeholders for the rest of the puzzles

---
+++ advent_of_code/2021/16.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+
+"""
+
+from collections.abc import Iterable
+
+
+def part_one(lines: Iterable[str]) -> int:
+    return 0
+
+
+def part_two(lines: Iterable[str]) -> int:
+    return 0
