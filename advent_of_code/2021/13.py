5bc4392064d3877383c56c874b415f94c573e747
John Flavin <flavinj@gmail.com>
2021-12-13 09:17:04 -0600

    Format with black

--- advent_of_code/2021/13.py
+++ advent_of_code/2021/13.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 """
 Part 1
-How many dots are visible after completing just the first fold instruction on your transparent paper?
+How many dots are visible after completing just the first fold
+instruction on your transparent paper?
+
+Part 2
+Finish folding the transparent paper according to the instructions.
+The manual says the code is always eight capital letters.
 """
 
 from collections.abc import Iterable
@@ -67,11 +72,11 @@ FoldFunc = Callable[[Pt], Pt]
 
 
 def fold_x(pt: Pt, value: int) -> Pt:
-    return pt if pt.x < value else Pt(2*value - pt.x, pt.y)
+    return pt if pt.x < value else Pt(2 * value - pt.x, pt.y)
 
 
 def fold_y(pt: Pt, value: int) -> Pt:
-    return pt if pt.y < value else Pt(pt.x, 2*value - pt.y)
+    return pt if pt.y < value else Pt(pt.x, 2 * value - pt.y)
 
 
 def parse(lines: Iterable[str]) -> tuple[list[Pt], list[FoldFunc]]:
@@ -110,7 +115,7 @@ def to_str(pts: set[Pt]) -> str:
             max_x = pt.x
         if pt.y > max_y:
             max_y = pt.y
-    empty_line = ["."]*(max_x + 1)
+    empty_line = ["."] * (max_x + 1)
     array = [list(empty_line) for _ in range(max_y + 1)]
     for pt in pts:
         array[pt.y][pt.x] = "#"
@@ -119,9 +124,11 @@ def to_str(pts: set[Pt]) -> str:
 
 def part_two(lines: Iterable[str]) -> str:
     pts, folds = parse(lines)
+
     def fold_pt(pt: Pt) -> Pt:
         for func in folds:
             pt = func(pt)
         return pt
+
     folded_pts = set(map(fold_pt, pts))
     return to_str(folded_pts)
