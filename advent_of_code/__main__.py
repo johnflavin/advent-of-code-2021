13a707e2075d3389474d7cb62cd1ded9c50f738f
John Flavin <flavinj@gmail.com>
2021-12-19 08:59:18 -0600

    Add logging configuration to __main__. I intend to add a command line arg to control this later.

--- advent_of_code/__main__.py
+++ advent_of_code/__main__.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 import sys
 from datetime import datetime
 
@@ -126,5 +127,7 @@ def main(argv):
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.ERROR)
+    # logging.getLogger(__package__).setLevel(logging.DEBUG)
     ret = main(sys.argv[1:])
     sys.exit(ret)
